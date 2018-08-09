5e91b60d4f09bb9ee8241d9adce0c0ba4f193333
Sean Lee <iseansay@gmail.com>
2018-08-09 15:07:11 +0800

    oh bad

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,21 +4,16 @@ import { MemoryRouter as Router} from 'react-router'
 import Places from "../components/places"
 import Front from "../components/front"
 import End from "../components/end"
-import NotFound from "./404"
-import { withRouter} from 'react-router'
-import { createMemoryHistory } from 'history'
 
 import "./main.css"
 
-const history = createMemoryHistory()
-
 const App = () => (
   <div>
-    <Router history={history}>
+    <Router>
       <div>
-          <Route exact path="/" component={Front}/>
           <Route path="/places" component={Places} />
           <Route path="/end" component={End} />
+          <Route exact path="/" component={Front}/>
       </div>
     </Router>
   </div>
